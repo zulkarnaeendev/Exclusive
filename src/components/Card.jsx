4c59ff8add1f2b8c7f9b7f22a1e8b08d729c7789
Zulkarnaeendev <Zulkarnaeendev@gmail.com>
2026-02-15 21:59:31 +0600

    Final code ready to be committed

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,8 +1,7 @@
 import React from "react";
-<<<<<<< HEAD
-import { FaRegHeart, FaEye } from "react-icons/fa"; 
+import { FaRegHeart, FaEye } from "react-icons/fa";
 import { Rate } from 'antd';
-import { useNavigate } from "react-router"; 
+import { useNavigate } from "react-router";
 
 const Card = ({ img, Name, prevprize, prize, discount, review, display, id }) => {
 
@@ -13,29 +12,16 @@ const Card = ({ img, Name, prevprize, prize, discount, review, display, id }) =>
   };
 
   const handelAddToCart = () => {
-    
+
   }
 
-=======
-import { FaStar } from "react-icons/fa6";
-import { FaRegHeart } from "react-icons/fa";
-import { FaEye } from "react-icons/fa";
-import { Rate } from 'antd';
-
-const Card = ({ img, Name, prevprize, prize, discount, review, display }) => {
->>>>>>> de9f8e4bc59c9b427e938c22dde1da0b6a0d864e
   return (
     <div className="container mt-10 w-67.5 group">
       <div className="relative overflow-hidden bg-secondary rounded-md">
         <img
           src={img}
-<<<<<<< HEAD
-          alt="" 
-          className="w-67.5 h-62.5 group-hover:scale-105 will-change-transform bg-[#F5F5F5]"
-=======
           alt=""
-          className="w-67.5 h-62.5 group-hover:scale-105 will-change-transform"
->>>>>>> de9f8e4bc59c9b427e938c22dde1da0b6a0d864e
+          className="w-67.5 h-62.5 group-hover:scale-105 will-change-transform bg-[#F5F5F5]"
         />
         {discount && (
           <div className="text-xs font-medium text-white px-3 py-1 bg-red-500 w-fit absolute top-4 left-4 rounded-sm">
@@ -47,24 +33,14 @@ const Card = ({ img, Name, prevprize, prize, discount, review, display }) => {
             <FaRegHeart className="text-[20px]" />
           </div>
           <div className="bg-white rounded-full w-8.5 h-8.5 flex justify-center items-center hover:bg-red-500 hover:text-white transition-colors cursor-pointer">
-<<<<<<< HEAD
             <FaEye className="text-[20px]" onClick={handleProductDetail} />
-=======
-            <FaEye className="text-[20px]" />
->>>>>>> de9f8e4bc59c9b427e938c22dde1da0b6a0d864e
           </div>
         </div>
         {display && (
           <div className="absolute bottom-0 w-full h-10 bg-black opacity-0 group-hover:opacity-100 transition-opacity duration-300 cursor-pointer">
-<<<<<<< HEAD
             <button className="text-white py-2 px-22 h-full font-medium cursor-pointer" onClick={handelAddToCart}>
               Add To Cart
             </button>
-=======
-            <p className="text-white flex justify-center items-center h-full font-medium">
-              Add To Cart
-            </p>
->>>>>>> de9f8e4bc59c9b427e938c22dde1da0b6a0d864e
           </div>
         )}
       </div>
